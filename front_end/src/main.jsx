7e4792bae459fbfa53a3498f9e398cf60415d899
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-14 11:40:51 +0100

    initial commit

---
+++ front_end/src/main.jsx
@@ -0,0 +1,33 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+// promise.then(response => {
+//   console.log(response)
+// })
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <App notes={notes} />
+// )
