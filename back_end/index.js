f8ae0ed8d874eff24068091a92709b2fdff6fea7
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-14 12:34:19 +0100

    test1

--- back_end/index.js
+++ back_end/index.js
@@ -91,7 +91,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
